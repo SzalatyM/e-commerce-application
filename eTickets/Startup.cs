220222bda718cb16e2323c16505cb2e25b144759
Michał <miszalaty@gmail.com>
2023-04-14 16:50:26 +0200

    IActorsServices, implementation

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +29,11 @@ namespace eTickets
 
             // DbContext Configuration
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
+
+
+            //services configuration
+
+            services.AddScoped<IActorsService, ActorsService>();
             services.AddControllersWithViews();
         }
 
